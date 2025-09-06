cbb79c2a4fa6e7010e73a1bdea5bb212f43bccfa
MNLab-Rui <70370654+Ruixin-char113@users.noreply.github.com>
2025-09-06 22:47:16 +0800

    init

---
+++ Source/[RX]shieldTimer/shieldTimer.cs
@@ -0,0 +1,40 @@
+﻿// RimWorld\Data\Royalty\Defs\ThingDefs_Buildings\Buildings_Exotic.xml
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+
+// RimWorld.CompProjectileInterceptor
+namespace _RX_shieldTimer
+{
+    [StaticConstructorOnStartup]
+    public static class RXshieldTimer
+    {
+        static RXshieldTimer()
+        {
+            var harmony = new Harmony("ruixinchar113.shieldTimer");
+            harmony.PatchAll();
+            Log.Message("[RX] Shield Timer Loaded.");
+        }
+    }
+
+    [HarmonyPatch(typeof(CompProjectileInterceptor))]
+    [HarmonyPatch(nameof(CompProjectileInterceptor.CompTick))]
+    public static class CompProjectileInterceptor_CompTick_Patch
+    {
+        public static void Postfix(CompProjectileInterceptor __instance)
+        {
+            CompDestroyAfterDelay compDelay = __instance.parent.TryGetComp<CompDestroyAfterDelay>();
+            int leftTick = compDelay?.TicksLeft ?? 0;
+            if (leftTick > 0)
+            {
+                MoteMaker.ThrowText(__instance.parent.DrawPos, __instance.parent.Map, leftTick.ToStringSecondsFromTicks("F1"), 1 / 60f);
+            }
+        }
+    }
+}
